ba8c09b21bc3b2d85d821c4f48f7bf229ca22b20
Fatemeh <ahmadi@dbs.uni-hannover.de>
2023-08-14 10:53:47 +0200

    fixed exp running

--- Raha/raha/eds_run_experiments/raha_enough_labels_lake.py
+++ Raha/raha/eds_run_experiments/raha_enough_labels_lake.py
@@ -1,14 +1,15 @@
 import os
 import logging
 import time
+from raha.detection import main
 
-exp_name = "git-2"
-logging.basicConfig(filename=f'raha/logs/{exp_name}.log', level=logging.DEBUG)
+exp_name = "raha-enough-labels_1_25"
+logging.basicConfig(filename=f'/home/fatemeh/results-aug/raha-enough-labels/1_25/logs/{exp_name}.log', level=logging.DEBUG)
 
-repition = range(1, 2)
-labeling_budgets = range(2, 3)
-sandbox_path = "/home/fatemeh/ED-Scale-mp-dgov/ED-Scale/git-2-tables/tables"
-results_path = os.path.join("/home/fatemeh/EDS-BaseLines/Raha/raha/res-git-250", f"exp_{exp_name}")
+repetition = range(1, 6)
+labeling_budgets = [1, 5, 10, 15, 20]
+sandbox_path = "/home/fatemeh/EDS-Raha_exp/Raha/raha/datasets/output_lake_1_25_processed"
+results_path = os.path.join("/home/fatemeh/results-aug/raha-enough-labels/1_25", f"exp_{exp_name}")
 if not os.path.exists(results_path):
     os.makedirs(results_path)
 
@@ -19,20 +20,15 @@ for dir in os.listdir(sandbox_path):
 
 logging.info(f"datasets: {datasets}")
 
-def run_raha(dataset, results_path, labeling_budget, exec_number):
-    python_script = f'''python raha/detection.py \
-                             --results_path {results_path} --base_path {dataset} --dataset {os.path.basename(dataset)} --labeling_budget {labeling_budget} --execution_number {exec_number}'''
-    logging.info("python_script: " + python_script)
-    os.system(python_script)
-
 s_time = time.time()
-for exec_number in repition:
+for execution_number in repetition:
     for labeling_budget in labeling_budgets:
-        for dataset in datasets:
+        for dataset_path in datasets:
             try:
-                run_raha(dataset, results_path, labeling_budget, exec_number)
+                dataset_name = os.path.basename(dataset_path)
+                main(results_path, dataset_path, dataset_name, labeling_budget, execution_number)
             except Exception as e:
-                logging.error(dataset, e)
+                logging.error(dataset_path, e)
 e_time = time.time()
-print("Total Run Time:")
-print(e_time - s_time)
+logging.info("Total Run Time:")
+logging.info(e_time - s_time)
